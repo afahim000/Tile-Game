a3ad9bc7419aa4c010d7c3c6bdb4cb6ef5b49e24
Abrar Fahim <Abrarfahim100@gmail.com>
2025-10-26 10:26:08 -0400

    player

--- my-app/src/screen.jsx
+++ my-app/src/screen.jsx
@@ -1,4 +1,4 @@
-
+import {useState} from 'react'
 export default function Screen()
 {
     const grid = [
@@ -13,7 +13,7 @@ export default function Screen()
     
     <>
         <div className = "begin">
-            
+            <Player/>
             {grid.map((column)=>
             {
                 return(
@@ -27,6 +27,29 @@ export default function Screen()
     )
 }
 
+function Player()
+{
+
+    const [pos,setPos] = useState([1,0])
+    function moveX(val)
+    {
+        setPos(pos.map((elem)=>
+        {
+            return [elem[0]+val,elem[1]]
+        }))
+    }
+
+    function moveY(val)
+    {
+        setPos(pos.map((elem)=>
+        {
+            return [elem[0],elem[1]+val]
+        }))
+    }
+    return(
+        <div className = "player" style = {{outline: '1px solid red', borderRadius: '25px', height:'50px', width: '50px', margin: '0', boxSizing: 'border-box', position: 'absolute', left: pos[0]* 50, top: pos[1]* 50}}></div>
+    )
+}
 function Tile({set})
 {
     let zindex = 0;
@@ -34,7 +57,13 @@ function Tile({set})
         <>
             {set.map((tile)=>
             {
-                return( <div className = "tile" style = {{display: 'inline', position: 'absolute', left: zindex * 50, top: 0, zIndex: ++zindex}}></div>)
+                return( 
+                   
+                <div className = "tile" style = {{display: 'inline', position: 'absolute', left: zindex * 50, top: 0, zIndex: ++zindex, padding: '0'}}>
+                 
+                </div>
+                )
+                
             })}
         </>
       
@@ -45,7 +74,7 @@ function Column({children})
 {
     let zindex;
     return(
-        <div className = 'column' style = {{display: 'block', position: 'relative'}}>
+        <div className = 'column' style = {{display: 'block', position: 'relative', boxSizing: 'border-box'}}>
             {children}
         </div>
     )
